9624caa2a6773567304a66cfa122150b263f9155
jobhunter-neo <jobhunter.neo@gmail.com>
2022-02-06 21:09:28 +0530

    second commit

--- src/com/HaloNeo.java
+++ src/com/HaloNeo.java
@@ -5,6 +5,7 @@ public class HaloNeo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Halo Neo! Welcome to Java Programming");
+		System.out.println("Awesome Java Programming!!!");
 	}
 
 }
