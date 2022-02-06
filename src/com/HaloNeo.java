33f2a112d194b501f92fdb75989b366f23b4d46c
jobhunter-neo <jobhunter.neo@gmail.com>
2022-02-06 20:00:47 +0530

    first Java project commit

---
+++ src/com/HaloNeo.java
@@ -0,0 +1,10 @@
+package com;
+
+public class HaloNeo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Halo Neo! Welcome to Java Programming");
+	}
+
+}
